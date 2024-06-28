f68d019164508b6f918e0a3bc143fb5caf3410cf
nitinya9av <nitinya9av@gmail.com>
2024-06-28 16:11:57 +0530

    typingAnimation_Added

--- script.js
+++ script.js
@@ -32,4 +32,19 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Student","Open Source Contributor","Developer","Tech Enthusiast"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Student","Open Source Contributor","Developer","Tech Enthusiast"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 });
